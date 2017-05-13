a2695120f50d0865fcc9e315ae7c0ac555e73dca
Dmitry Patsura <talk@dmtry.me>
2017-05-13 21:07:33 +0900

    Implement client middleware

--- Client.js
+++ Client.js
@@ -13,49 +13,61 @@ function buildQueryString(params: Object): string {
         .join('&');
 }
 
-export function required(key: string) {
+function required(key: string) {
     throw new Error(`Missing required parameter: ${key}`);
 }
 
-export function request(url: string, params: Object, method: string = 'GET', options:? FetchOptions) {
-    return new Promise(
-        (resolve, reject) => {
-            let requestOptions = Object.assign({}, options, { method: method});
+function makeClient() {
+    let before = [];
+    let after = [];
 
-            if (method == 'POST' || method == 'PUT') {
-                if (requestOptions.headers) {
-                    requestOptions.headers = Object.assign(
-                        requestOptions.headers,
-                        {
-                            "Content-Type": "application/x-www-form-urlencoded"
-                        }
-                    );
-                } else {
-                    requestOptions.headers = {
-                        "Content-Type": "application/x-www-form-urlencoded"
-                    };
-                }
+    return {
+        before: (midlleware) => {
+            before.push(midlleware);
+        },
+        request: (url: string, params: Object, method: string = 'GET', options:? FetchOptions) => {
+            let requestOptions = Object.assign({}, options, { method: method });
 
-                requestOptions = Object.assign(
-                    requestOptions,
-                    {
-                        body: JSON.stringify(params)
-                    }
-                )
-            } else if (params && Object.keys(params).length > 0) { // Check that parameters exists
-                url += '?' + buildQueryString(params)
+            if (before) {
+                before.forEach((middleware) => middleware(requestOptions));
             }
 
-            fetch(API_BASE_PATH + url, requestOptions).then(
-                (response) => {
-                    if (response.status >= 200 && response.status < 300) {
-                        resolve(response.json());
-                    } else {
-                        reject(response);
+            return new Promise(
+                (resolve, reject) => {
+                    if (method == 'POST' || method == 'PUT') {
+                        requestOptions = Object.assign(
+                            requestOptions,
+                            {
+                                body: JSON.stringify(params)
+                            }
+                        )
+                    } else if (params && Object.keys(params).length > 0) { // Check that parameters exists
+                        url += '?' + buildQueryString(params)
                     }
-                },
-                (err) => reject(err)
+
+                    fetch(API_BASE_PATH + url, requestOptions).then(
+                        (response) => {
+                            if (response.status >= 200 && response.status < 300) {
+                                resolve(response.json());
+                            } else {
+                                reject(response);
+                            }
+                        },
+                        (err) => reject(err)
+                    )
+                }
             )
         }
-    )
+    }
+}
+
+const defaultClient = makeClient();
+const before = defaultClient.before;
+const request = defaultClient.request;
+
+export {
+    required,
+    makeClient,
+    before,
+    request
 }
