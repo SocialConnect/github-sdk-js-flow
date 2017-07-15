3d0648c0e684647232e309900144be5aadac5936
Dmitry Patsura <talk@dmtry.me>
2017-07-15 16:42:40 +0900

    [Regenerate] New method: getNotifications

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ export * from './Client';
 export * from './Auth';
 export * from './Repository';
 export * from './Search';
+export * from './Notifications.js';
 export * from './GitHub';
 export * from './User';
 export * from './GraphQL.js';
